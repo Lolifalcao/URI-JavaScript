8e307510f6434d35bb3bc3e25e67b9be57e8bec5
Carol Falcão <acarolinafalcao@gmail.com>
2021-10-16 17:59:09 -0300

    Exercício 4

---
+++ 1004 - Produto Simples/1004.js
@@ -0,0 +1,12 @@
+let num1 = prompt('Digite o primeiro valor');
+let num2 = prompt('Digite o segundo valor');
+
+console.log(num1);
+console.log(num2);
+
+let prod = num1 * num2;
+console.log(prod);
+
+document.write('PROD = ',prod);
+
+alert('PROD = ' + prod);
