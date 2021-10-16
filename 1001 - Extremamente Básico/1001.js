5b1f543f32dea9c364cb3cc340c184fdf2692779
Carol Falcão <acarolinafalcao@gmail.com>
2021-10-16 14:09:00 -0300

    Commitando apenas o Javascript

---
+++ 1001 - Extremamente Básico/1001.js
@@ -0,0 +1,16 @@
+let a = prompt('Escreva sua pontuação A');
+let b = prompt('Escreva sua pontuação B');
+
+let numa = parseInt(a);
+let numb = parseInt(b);
+
+console.log(numa);
+console.log(numb);
+
+let x = numa + numb;
+console.log('X = '+ x);
+console.log('X =', x);
+
+// imprimindo no html
+
+document.write('X = ', x);
