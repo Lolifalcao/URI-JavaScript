202fcbd00250853470c44703328ee1890cbaf6d1
Carol Falcão <acarolinafalcao@gmail.com>
2021-10-16 17:22:56 -0300

    Exercício 4

---
+++ 1003 - Soma simples/1003.js
@@ -0,0 +1,13 @@
+let a = prompt('Digite o primeiro valor')
+let b = prompt('Digite o segundo valor')
+
+let A = parseInt(a)
+let B = parseInt(b)
+
+console.log(A)
+console.log(B)
+
+let SOMA = A + B
+console.log(SOMA)
+
+document.write('SOMA =', SOMA)
