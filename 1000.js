05b191c4debe055fb048f3f0255111ec04338480
Carol Falcão <acarolinafalcao@gmail.com>
2021-10-16 12:32:53 -0300

    meu primeiro commit

---
+++ 1000.js
@@ -0,0 +1,12 @@
+// O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". 
+// Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+console.log('Hello world')
+
+let saudacao = 'Bem vindo'
+
+console.log(saudacao)
+
+// imprimindo no html
+
+document.write('Hello world')
