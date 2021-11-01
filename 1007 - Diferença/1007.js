c64a904493fcc17a37705ddf8b86aa0e16baa3d3
Carol Falcão <acarolinafalcao@gmail.com>
2021-11-01 10:10:42 -0300

    Exercício 1007

---
+++ 1007 - Diferença/1007.js
@@ -0,0 +1,10 @@
+let valorA = parseInt(prompt('Digite valor A'));
+let valorB = parseInt(prompt('Digite valor B'));
+let valorC = parseInt(prompt('Digite valor C'));
+let valorD = parseInt(prompt('Digite valor D'));
+console.log(valorA+'\n',valorB+'\n',valorC+'\n',valorD);
+
+let DIFERENCA = ((valorA*valorB)-(valorC*valorD));
+console.log(DIFERENCA);
+
+document.write('DIFERENCA = ',DIFERENCA);
